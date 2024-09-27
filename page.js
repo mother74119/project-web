c06c9e7c185115bbfcec412ef803cc14ff441c1d
mother74119 <zee0816556428@gmail.com>
2024-09-27 10:11:55 +0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import {useState} from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function Home() {
+
+  return (
+    <>
+     <h1>Hello home</h1>
+    </>
+  )
+}
